a353a9fd0eeb0a9afb40d144eb26b8f3e0fd0535
shofadzik <107288295+shofadzik@users.noreply.github.com>
2022-09-01 05:52:23 +0700

    Create soal.py

---
+++ soal.py
@@ -0,0 +1,12 @@
+/*
+Sebagai marketing leader, saya ingin dashboard report berikut:
+
+(gross revenue = (harga-diskon)* jumlah barang)
+
+1. column chart supplier (company_name) gross revenue tiap bulan  (datamart_monthly_supplier_gross_revenue_<nama>)
+2. tabel kategori produk paling banyak terjual tiap bulan 
+(datamart_monthly_category_sold_<nama>)
+3. geo map negara (ship_country) pembelian berdasarkan gross revenue per bulan (datamart_monthly_country_gross_revenue_<nama>)
+4. line chart gross revenue total per hari dalam satu bulan
+(datamart_daily_gross_revenue_<nama>)
+*/
